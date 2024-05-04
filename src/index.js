95f9015c3d48d8442fad4abe4c04cc6469dd56e2
NIKHIL SHARMA <nikhilsharma302@gmail.com>
2024-05-04 20:57:33 +0530

    code added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const reader = async (fileName) => {
-  // write code here
-	// dont change function name
-  
+  const text=await fs.readFile(fileName)
+  return text
 };
 
 
